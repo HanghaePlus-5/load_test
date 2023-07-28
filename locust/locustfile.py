c3c08bc1efaaa6684c8f34213ab60edfee6ca173
ninthsun91 <ninthsun91@gmail.com>
2023-07-29 00:48:46 +0900

    :recycle: Apply sleep at the beginning
    - spread requests

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -7,6 +7,8 @@ import time
 class plus_load(HttpUser):
     @task
     def plus_load(self):
+        time.sleep(random.random() * 2)
+
         now = math.floor(time.time() * 100000)
         rand = math.floor(random.random() * 100000)
         uid = str(now)[7:] + str(rand)
@@ -22,29 +24,30 @@ class plus_load(HttpUser):
         headers = {
             'content-type': 'application/json'
         }
-        self.client.post('/api/v1/users/signup', data=json.dumps(signup_form), headers=headers)
-
-        time.sleep(random.random() * 5)
+        res = self.client.post('/api/v1/users/signup', data=json.dumps(signup_form), headers=headers)
+        if res.ok == False: return
         
         login_form = {
             'email': f'user_{uid}@plus.com',
             'password': 'qwe123123'
         }
         res = self.client.post('/api/v1/users/signin', data=json.dumps(login_form), headers=headers)
-        access_token = res.cookies.get('accessToken')
+        if res.ok == False: return
 
         time.sleep(random.random() * 5)
 
+        access_token = res.cookies.get('accessToken')
         headers = {
             'content-type': 'application/json',
             'authorization': f'Bearer {access_token}'
         }
         res = self.client.get('/api/v1/stores/search?keyword=&page=1&limit=10', headers=headers)
-        store_data = res.json()
-        store = random.choice(store_data['data'])
+        if res.ok == False: return
 
         time.sleep(random.random() * 5)
 
+        store_data = res.json()
+        store = random.choice(store_data['data'])
         order_form = {
             'storeId': store['storeId'],
             'orderItem': [
@@ -60,3 +63,5 @@ class plus_load(HttpUser):
         }
         data = json.dumps(order_form)
         self.client.post('/api/v1/orders', data=data, headers=headers)
+
+        time.sleep(random.random() * 3)
